827091b7bda9340d90374d26c9a3955aea968df9
gabriel kolber <gabrielkolber@icloud.com>
2021-01-22 00:02:48 -0300

    feat: added fizz buzz function and pipeline

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 **TODO: Add description**
 
+## The Problem
+"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+
+
 ## Installation
 
 If [available in Hex](https://hex.pm/docs/publish), the package can be installed
--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -1,18 +1,41 @@
 defmodule FizzBuzz do
-  @moduledoc """
-  Documentation for `FizzBuzz`.
-  """
-
   @doc """
   Hello world.
 
   ## Examples
 
-      iex> FizzBuzz.hello()
+      iex> FizzBuzz.fizzbuzz()
       :world
 
   """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
+
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |>String.split(",")
+      |>Enum.map(&convert_and_verify/1)
+
+      {:ok , result}
+
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error,"Error reading the file: #{reason}"}
+
+  defp convert_and_verify(value) do
+    value
+    |>String.to_integer()
+    |>evaluate_fizz_buzz()
+  end
+
+  defp evaluate_fizz_buzz(number) when rem(number,3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_fizz_buzz(number) when rem(number,3) == 0, do: :fizz
+  defp evaluate_fizz_buzz(number) when rem(number,5) == 0, do: :buzz
+  defp evaluate_fizz_buzz(number), do: number
+
+
 end
